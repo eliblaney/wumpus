c49185338038ac31672df11d9e974b0876d826c6
Eli Blaney <eliblaney@gmail.com>
2021-04-24 11:19:24 -0500

    Add movement

--- main.lua
+++ main.lua
@@ -4,15 +4,70 @@ function love.load()
 	love.window.setTitle('Hunt the Wumpus')
 
 	caveImg = love.graphics.newImage("images/cave.png")
-	playerImg = love.graphics.newImage("images/character.png")
+
+	player = {}
+	player.img = love.graphics.newImage("images/character.png")
+	player.x = 300
+	player.y = 550
+	player.scale = 0.5
+	player.speed = 300
+	player.canMove = true
+
+	scene = {}
+	scene.minX = 200
+	scene.maxX = 600
+	scene.minZ = 0.3
+	scene.maxZ = 0.7
+	scene.dim = 0
 	
 -- 	sound = love.audio.newSource("music.ogg", "stream")
 -- love.audio.play(sound)
 end
 
+function love.update(dt)
+	-- detect go to cave
+	caveDir = -1
+	if player.x <= scene.minX then caveDir = 0 end
+	if player.x >= scene.maxX then caveDir = 1 end
+	if player.scale <= scene.minZ then caveDir = 2 end
+	if player.scale >= scene.maxZ then caveDir = 3 end
+	if caveDir > -1 or scene.dim < 0 then
+		player.canMove = false
+		scene.dim = scene.dim + 0.05
+	end
+
+	if scene.dim >= 1 then
+		-- change cave in complete darkness
+		scene.dim = -1
+		player.x = 300
+		player.y = 550
+		player.scale = 0.5
+		-- TODO: Change cave
+	end
+	if scene.dim >= -0.1 and scene.dim < 0 then
+		player.canMove = true
+		scene.dim = 0
+	end
+
+	-- movement
+	if player.canMove then
+		if love.keyboard.isDown("left") then
+			player.x = player.x - player.speed*dt
+		elseif love.keyboard.isDown("right") then
+			player.x = player.x + player.speed*dt
+		elseif love.keyboard.isDown("up") then
+			player.scale = player.scale - player.speed*dt/200
+		elseif love.keyboard.isDown("down") then
+			player.scale = player.scale + player.speed*dt/200
+		end 
+	end
+end
+
 function love.draw()
-	love.graphics.print("Hello World!", 400, 300)
+	love.graphics.setColor(1, 1, 1, 1)
 	love.graphics.draw(caveImg, 0, 0)
-	love.graphics.draw(playerImg, 300, 300, 0, 0.5, 0.5)
+	love.graphics.draw(player.img, player.x, player.y, 0, player.scale, player.scale, 250, 450)
+	love.graphics.setColor(0, 0, 0, math.abs(scene.dim))
+	love.graphics.rectangle('fill', 0, 0, 808, 800)
 end
 
